602bc4209839b45b52a28ad53e66f23765a41f59
ashikmahmud <ashikmamud187@gmail.com>
2022-06-10 17:40:36 +0600

    putting dynamic data from api

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,3 +13,32 @@ dashboardBtn.addEventListener("click", ()=>{
         dashboardBtn.querySelector("i").classList.add("bx-menu")
     }
 })
+
+
+/* Showing All the Task  */
+
+// Get Task Title 
+const getTaskTitle = async() =>{
+    const data = await fetch(`data.json`);
+    const result = await data.json();
+    const taskTitleArray = result?.tasks[0]?.assets.map(task => task.asset_title);  
+    const tasksAssets = result?.tasks[0]?.assets;  
+    return {taskTitleArray, tasksAssets};  
+}
+// showing all title on dashboard 
+const showTaskContent = async() =>{
+    const contentBoxTitle = document.querySelectorAll(".content-box__header");
+    const {taskTitleArray, tasksAssets} = await getTaskTitle();   
+    const taskTitle = document.querySelector(".task-title-area");
+    taskTitle.innerHTML = taskTitleArray.map(title => `<li><a href="">${title}</a></li>`).join("");
+    contentBoxTitle.forEach((title, ind) => {        
+      title.innerHTML = taskTitleArray[ind];       
+    })
+   
+    document.querySelector(".content-box__body p").innerText = tasksAssets[0]?.asset_description;
+}
+showTaskContent();
+
+
+
+
